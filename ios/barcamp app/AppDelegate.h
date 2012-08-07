18fd828538b026f47cadf0ce5b57a709082fff47
Álvaro Aroca <alvaro.aroca.m@gmail.com>
2012-08-07 19:20:47 +0200

    Creado proyecto de iOS (con Core Data)

---
+++ ios/barcamp app/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  barcamp app
+//
+//  Created by Álvaro Aroca Munoz on 07/08/12.
+//  Copyright (c) 2012 Álvaro Aroca Munoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ios/barcamp app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  barcamp app
+//
+//  Created by Álvaro Aroca Munoz on 07/08/12.
+//  Copyright (c) 2012 Álvaro Aroca Munoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
